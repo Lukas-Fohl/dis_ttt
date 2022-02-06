da1c10a3d722b8e847f7d03d20b7e79edd070ced
Lukas Fohl <Lukas@fohl.net>
2022-02-06 18:20:48 +0100

    json

--- bot.py
+++ bot.py
@@ -3,14 +3,16 @@ from distutils.util import change_root
 from email import message, message_from_string
 from operator import truediv
 from pickle import FALSE, TRUE
-from syslog import LOG_USER
+import winreg
 import discord
 import os
 import json
 from re import search
 
-TOKEN = ''
+TOKEN = 'ODkzNTY4OTQ4MDgyMjAwNjA4.YVdW7g.mPJcJE4UhgI7EfiqYZ1sTRoSbio'
 client = discord.Client()
+global json_file_path
+json_file_path = os.path.dirname(os.path.abspath(__file__)) +"\main.json"
 
 global new_match
 new_match = False
@@ -28,7 +30,7 @@ async def on_ready():
     global match_is_playing
     match_is_playing = False
     print('I\'m {0.user}'.format(client))
-    game = discord.Game("nicht genshin impact")
+    game = discord.Game("TicTacToe")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
@@ -44,7 +46,6 @@ async def on_message(message):
         global match_is_playing
         global new_match
         global player_now
-        print(match_is_playing)
         if user_message == "new game" and new_match == False and match_is_playing == False:
             await message.channel.send(f'new match ist starting (join with join)')
             new_match = True
@@ -53,11 +54,9 @@ async def on_message(message):
             if player[0] == "":
                 player[0] = username
                 await message.channel.send(f'{username} joined')
-                print(username)
             elif player[1] == "":
                 player[1] = username
                 await message.channel.send(f'{username} joined')
-                print(username)
 
             if player[0] != "" and player[1] != "":
                 match_is_playing = True
@@ -67,7 +66,6 @@ async def on_message(message):
             return
         elif search("place",user_message) and match_is_playing == True and match_is_playing == True:
             if username == player[0] and player_now == player[0]:
-                print(int(user_message[6]))
                 if board[int(user_message[6])] == 0:
                     board[int(user_message[6])] = 8
                 player_now = player[1]
@@ -84,91 +82,90 @@ async def on_message(message):
         elif user_message == "user":
             await print_user(message, username) 
         elif user_message == "help":
-            await message.channel.send("-new game\t-makes new game \n-join\t\t\t\t -you join an open game\n-place\t\t\t  -places your point at the a point")
+            await message.channel.send("-new game\t -makes new game \n-join\t\t\t\t -you join an open game\n-place\t\t\t  -places your point at the a point\n-user\t\t\t\t-shows scores")
 
+def look_for_ex(username):
+    global json_file_path
+    with open(json_file_path,"r") as json_file:
+        data = json.load(json_file)
+        for i in data["player"]:
+            if i['name'] == username:
+                return
+    new_user(username)
+    return
+
+
+def new_user(username):
+    new_player ={
+            "name": username,
+            "won": 0,
+            "lost":0,
+            "draw":0
+        }
+
+    with open(json_file_path,"r+") as json_file:
+        file_data = json.load(json_file)
+        file_data["player"].append(new_player)
+        json_file.seek(0)
+        json.dump(file_data, json_file, indent = 4)
+    return
 
 async def print_user(message_in, username):
+    look_for_ex(username)
     global won
     won = 0
     global lost
     lost = 0
     global draw
     draw = 0
-    path = os.path.realpath(__file__)
-    file = open(path, 'r')
-    lines = file.read.splite('\n')
-    for x in range(len(lines)):
-        if lines[x].split('#')[0] == username:
-            won = lines[x].split('#')[1]
-            lost = lines[x].split('#')[1]
-            draw = lines[x].split('#')[1] 
-    file.close()
 
-    #########Idea for json
-
-    f = open('main.json')
-    data = json.load(f)
-    for i in data.json()['player']:
-        if i['name'] == username:
-            won = i['won']
-            lost = i['lost']
-            draw = i['draw']
-            break
-    f.close()
-    await message_in.channel.send(f"macthes won:{won} matches lost {lost} matches ended in a draw{draw}")
+    with open(json_file_path,"r") as json_file:
+        data = json.load(json_file)
+        for i in data["player"]:
+            if i['name'] == username:
+                won = i['won']
+                lost = i['lost']
+                draw = i['draw']
+                break
+    await message_in.channel.send(f"macthes won {won} matches lost {lost} matches ended in a draw {draw}")
     return
 
 async def add_score(messsage_in, winner):
-    global change_file
-    change_file = NULL
+    look_for_ex(player[winner])
     global loser 
+    global change_1
+    change_1 = NULL
+    global change_2
+    change_2 = NULL
     if winner == 0:
+        change_1 = 'won'
+        change_2 = 'lost'
         loser = 1
     elif winner == 1:
+        change_1 = 'lost'
+        change_2 = 'won'
         loser = 0
-    if winner == 3:
-        global draw_1
-        draw_1 = 0
-        global draw_2
-        draw_2 = 0
-        f = open('main.json')
-        data = json.load(f)
-        for i in data.json()['player']:
+    elif winner == 3:
+        change_1 = 'draw'
+        change_2 = 'draw'
+        winner = 0
+        loser = 1
+    with open(json_file_path,"r+") as json_file:
+        data = json.load(json_file)
+        for i in data["player"]:
             if i['name'] == player[winner]:
-                draw_1 = i['draw']
-                break
-        for i in data.json()['player']:
+                i[change_1] = i[change_1]+1
+                json_file.seek(0)
+                json.dump(data, json_file, indent = 4)
+                break 
+    with open(json_file_path,"r+") as json_file:
+        data = json.load(json_file)
+        for i in data["player"]:
             if i['name'] == player[loser]:
-                draw_2 = i['draw']
-                break
-        f.close()
-        change_file['player'][player[winner]]['draw'] = draw_1+1
-        f = open('main.json')
-        json.dump(change_file,f)
-        change_file['player'][player[loser]]['draw'] = draw_2+1
-        json.dump(change_file,f)
-        f.close()
-        return
-
-    already_won = 0
-    already_lost = 0
-    f = open('main.json')
-    data = json.load(f)
-    for i in data.json()['player']:
-        if i['name'] == player[winner]:
-            already_won = i['won']
-            break
-    for i in data.json()['player']:
-        if i['name'] == player[loser]:
-            already_lost = i['lost']
-            break
-    f.close()
-    change_file['player'][player[winner]]['won'] = already_won+1
-    f = open('main.json')
-    json.dump(change_file,f)
-    change_file['player'][player[loser]]['lost'] = already_lost+1
-    json.dump(change_file,f)
-    f.close()
+                i[change_2] = i[change_2]+1
+                json_file.seek(0)
+                json.dump(data, json_file, indent = 4)
+                break           
     return
 
 async def look_for_win(message_in):
@@ -202,17 +199,13 @@ async def look_for_win(message_in):
         have_to_win_p2 = 0
     # other (yeah i know the word, do you?)  (Top right -> bottom left)
     if board[2] == 8 and board[4] == 8 and board[6] == 8:
-        print()
         await win(message_in,0)
     elif board[2] == 4 and board[4] == 4 and board[6] == 4:
-        print()
         await win(message_in,1)
     # other (yeah i know the word, do you?)  (Top left -> bottom right)
     if board[0] == 8 and board[4] == 8 and board[8] == 8:
-        print()
         await win(message_in,0)
     elif board[0] == 4 and board[4] == 4 and board[8] == 4:
-        print()
         await win(message_in,1)
     #look for draw / full board
     placed = 0
